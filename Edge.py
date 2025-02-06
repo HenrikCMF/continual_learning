2b0652de9b9725eb2db71c2d33d8806d24e58535
Henrik <theredjawa22@gmail.com>
2025-02-06 11:32:30 +0100

    class start

--- Edge.py
+++ Edge.py
@@ -1,12 +1,21 @@
 from TCP_code import TCP_COM
 import time
 import json
-with open("configs.json", "r") as file:
-    configs = json.load(file)
-local_IP=configs['edgeip']
-PORT=configs['PORT']
-rec_ip=configs['baseip']
-com_obj=TCP_COM(local_IP, PORT, rec_ip, PORT)
 
-while True:
-    time.sleep(0.1)
+class edge_device(TCP_COM):
+    def __init__(self, REC_FILE_PATH):
+        with open("configs.json", "r") as file:
+            configs = json.load(file)
+        self.local_IP=configs['edgeip']
+        self.PORT=configs['PORT']
+        self.rec_ip=configs['baseip']
+        super().__init__(self.local_IP, self.PORT, self.rec_ip, self.PORT, REC_FILE_PATH)
+
+    def receive_file(self, waittime=10):
+        time.sleep(waittime)
+        self.send_file("307.jpg")
+
+
+bs=edge_device("received")
+bs.send_file("307.jpg")
+bs.receive_file()
