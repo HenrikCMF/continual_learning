c84b52e055f31f0c0fb1d12adb1bc7b25b90020f
Henrik <theredjawa22@gmail.com>
2025-03-08 11:39:16 +0100

    sdj

---
+++ base_reset_network.py
@@ -0,0 +1,6 @@
+import subprocess
+import json
+with open("configs.json", "r") as file:
+        configs = json.load(file)
+interface=configs['baseNET_INTERFACE']
+subprocess.run(f"sudo tc qdisc del dev {interface} root", shell=True)
