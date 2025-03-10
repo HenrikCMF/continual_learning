5ac9612b8372b91a6152639dfb2e7fcbb8f638a5
Henrik <theredjawa22@gmail.com>
2025-03-10 15:08:04 +0100

    hu

--- Edge.py
+++ Edge.py
@@ -7,11 +7,13 @@ import pandas as pd
 import numpy as np
 import AVRO
 import os
-from queue import Queue
+import queue
+import shutil
 class edge_device(TCP_COM):
     def __init__(self, REC_FILE_PATH):
         self.total_sent_data=0
         self.device_type="edge"
+        self.model_path="model"
         with open("configs.json", "r") as file:
             configs = json.load(file)
         self.local_IP=configs['edgeip']
@@ -32,7 +34,7 @@ class edge_device(TCP_COM):
             jitter_ms=None
             self.nc.set_network_conditions(rate_kbps, burst_kbps, latency_ms, packet_loss_pct, delay_ms, jitter_ms)
         edgePORT=(self.edgePORT_TCP, self.edgePORT_UDP)
-        self.file_Q=Queue()
+        self.file_Q=queue.Queue()
         super().__init__(self.local_IP, edgePORT, self.rec_ip, self.basePORT, REC_FILE_PATH, self.device_type, self.file_Q)
         self.filename=make_dataset(1, 1)
         df=pd.read_csv(self.filename)
@@ -48,26 +50,37 @@ class edge_device(TCP_COM):
         sample_buffer=[]
         timestamp_buffer=[]
         while True:
-            s, t=self.get_sample()
-            if self.index%10000==0: #Replace with model MSE wrapper
-                sample_buffer.append(s)
-                timestamp_buffer.append(t)
-            if len(timestamp_buffer)>5 or self.index==self.len_of_dataset: #Replace 5 with variable network parameter
-                sample_buffer=np.array(sample_buffer)
-                filename=os.path.join(
-                    'test_files',
-                    str(timestamp_buffer[0]).replace(" ", "-").replace(":", "-")+'.avro'
-                    )
-                AVRO.save_AVRO_default(sample_buffer, timestamp_buffer,self.schema_path, accuracy=10,path=filename, original_size=len(sample_buffer), codec='deflate')
-                self.total_sent_data+=os.path.getsize(filename)
-                self.send_file(filename)
-                sample_buffer=[]
-                timestamp_buffer=[]
-                if self.index==self.len_of_dataset:
-                    time.sleep(100)
-                    exit()
+            try:
+                file = self.file_Q.get(timeout=0)
+                if ".tflite" in file:
+                    self.received_model(file)
+                self.file_Q.task_done()
+            except queue.Empty:
+                time.sleep(0.01)
+                s, t=self.get_sample()
+                if self.index%10000==0: #Replace with model MSE wrapper
+                    sample_buffer.append(s)
+                    timestamp_buffer.append(t)
+                if len(timestamp_buffer)>5 or self.index==self.len_of_dataset: #Replace 5 with variable network parameter
+                    sample_buffer=np.array(sample_buffer)
+                    filename=os.path.join(
+                        'test_files',
+                        str(timestamp_buffer[0]).replace(" ", "-").replace(":", "-")+'.avro'
+                        )
+                    AVRO.save_AVRO_default(sample_buffer, timestamp_buffer,self.schema_path, accuracy=10,path=filename, original_size=len(sample_buffer), codec='deflate')
+                    self.total_sent_data+=os.path.getsize(filename)
+                    self.send_file(filename)
+                    sample_buffer=[]
+                    timestamp_buffer=[]
+                    if self.index==self.len_of_dataset:
+                        time.sleep(100)
+                        exit()
         
-
+    def received_model(self, path):
+        if not os.path.exists(self.model_path):
+            os.makedirs(self.model_path)
+        destination_path=os.path.join(self.model_path, 'autoencoder.tflite')
+        shutil.move(path, destination_path)
     
     def get_sample(self):
         #should fetch the next sample in the dataset
