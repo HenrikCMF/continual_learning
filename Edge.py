0a4132fe29ea7144bbc8cef1330258fd0f0e993d
Henrik <theredjawa22@gmail.com>
2025-03-12 13:38:15 +0100

    ds

--- Edge.py
+++ Edge.py
@@ -65,7 +65,7 @@ class edge_device(TCP_COM):
             except queue.Empty:
                 if done_sending==False:
                     s, t=self.get_sample()
-                    mse=self.model.calc_mse(s)
+                    mse=self.model.calc_mse(s.drop(columns=["machine_status"]))
                     mse_buff.append(mse)
                     if self.index==self.len_of_dataset:
                         print("done")
