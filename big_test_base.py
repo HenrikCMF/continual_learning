2e0c2a1e0294a887e8441825a6af7987a29509e3
Henrik <theredjawa22@gmail.com>
2025-04-20 13:08:03 +0200

    hujds

--- big_test_base.py
+++ big_test_base.py
@@ -3,7 +3,7 @@ import numpy as np
 import time
 import csv
 import os
-file_path = "plots/pruning_results_size_test.csv"
+file_path = "plots/pruning_results_quantized.csv"
 
 if not os.path.isfile(file_path):
     with open(file_path, mode="a", newline="") as file:
