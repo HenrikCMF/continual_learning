d068b321e564efa3782c962191a395babdf9ce8f
Henrik <theredjawa22@gmail.com>
2025-04-24 09:21:24 +0200

    uihdfs

--- Edge.py
+++ Edge.py
@@ -46,7 +46,7 @@ class edge_device(TCP_COM):
             #packet_loss_pct=None
             delay_ms=None
             jitter_ms=None
-            #self.nc.set_network_conditions(rate_kbps, burst_kbps, latency_ms, packet_loss_pct, delay_ms, jitter_ms)
+            self.nc.set_network_conditions(rate_kbps, burst_kbps, latency_ms, packet_loss_pct, delay_ms, jitter_ms)
         edgePORT=(self.edgePORT_TCP, self.edgePORT_UDP)
         self.file_Q=queue.Queue()
         super().__init__(self.local_IP, edgePORT, self.rec_ip, self.basePORT, REC_FILE_PATH, self.device_type, self.file_Q)
