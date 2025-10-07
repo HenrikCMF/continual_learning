aa264015528dc87d6356062aedf12dc873db737c
Henrik <theredjawa22@gmail.com>
2025-10-07 19:50:16 +0200

    ifjdA

--- big_test_base.py
+++ big_test_base.py
@@ -3,7 +3,7 @@ import numpy as np
 import time
 import csv
 import os
-file_path = "plots/adapttest5.csv"
+file_path = "plots/adapttest6.csv"
 #Function for runnning the full test run multiple times but with a changing input for each run
 if not os.path.isfile(file_path):
     with open(file_path, mode="a", newline="") as file:
