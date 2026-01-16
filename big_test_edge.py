21718a3a6d91a1b0b1cb407f7e9bf6c3a4c9f86c
Henrik <hcmf@es.aau.dk>
2026-01-16 10:07:24 +0100

    deepiot energy test

--- big_test_edge.py
+++ big_test_edge.py
@@ -4,7 +4,7 @@ import time
 import csv
 import os
 import tensorflow as tf
-file_path = "plots/bandwidthtest_deep02.csv"
+file_path = "plots/energytest_deep02.csv"
 
 #Function for runnning the full test run multiple times but with a changing input for each run
 def analyze_model_energy_params(model_path):
@@ -55,9 +55,9 @@ if not os.path.isfile(file_path):
         writer = csv.writer(file)
         writer.writerow(["i","time_transmitting", "time_receiving", "total_sent_data", "total_received_data", "num_inferences", "measthrough", "energy"])
 
-start = 100
-stop = 1100
-step = 200
+start = 10
+stop = 70
+step = 10
 num_steps = int((stop - start) / step) + 1
 for idx in range(num_steps):
     i = round(start + step * idx, 2)
