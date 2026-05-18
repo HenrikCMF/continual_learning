ece721df94af4da8b97b5145d9e50d333ad67945
Henrik <hcmf@es.aau.dk>
2026-05-18 11:58:05 +0200

    test5

--- IoT_device.py
+++ IoT_device.py
@@ -101,12 +101,11 @@ class iot_device(TCP_COM):
         print(1.1)
         s, t=self.get_sample()   
         print(1.2) 
-        if self.inference_batch==0:
-            print(1.3)
-            for_mse=np.array(s.drop(self.configs['data_columns']['dataset_label'])).reshape(1,-1)
-        else:
-            print(1.4)
-            for_mse=s.drop(columns=self.configs['data_columns']['dataset_label'])
+
+
+        for_mse=np.array(s.drop(self.configs['string_configs']['data_columns']['dataset_label'])).reshape(1,-1)
+
+
         print(1.5)
         rare, mse=self.model.check_sample(for_mse)
         #rare=True
