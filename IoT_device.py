920d6d5630e8058b0fd09e76a500aaac0af088a2
Henrik <hcmf@es.aau.dk>
2026-05-18 20:30:46 +0200

    test2

--- IoT_device.py
+++ IoT_device.py
@@ -334,5 +334,5 @@ class iot_device(TCP_COM):
     
 
 if __name__ == "__main__":
-    es=iot_device("received", bandwidth=1000, energy_budget=300)
+    es=iot_device("received", bandwidth=1000, energy_budget=3000)
     es.run()
