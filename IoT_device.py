4d82d3603ee8ab244942c60893880467f38894d0
Henrik <hcmf@es.aau.dk>
2026-05-22 09:10:13 +0200

    ready

--- IoT_device.py
+++ IoT_device.py
@@ -147,8 +147,9 @@ class iot_device(TCP_COM):
             self.model.trigger_threshold=self.select_threshold(self.throughput)
         else:
             self.model.trigger_threshold=0.2
+        
         skip_samples=0
-        print("Throughput ", self.throughput, "NUMSAMPLES: ", NUM_BUF_SAMPLES, "Skipping ", skip_samples)
+        print("Throughput ", self.throughput, "NUMSAMPLES: ", NUM_BUF_SAMPLES, "Skipping ", skip_samples, "Threshold ", self.model.trigger_threshold)
         #time.sleep(0.01)
         while batch_not_found:
 
