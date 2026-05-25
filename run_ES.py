cc55ca84d71008dc1cb5062618ac02661962ce24
Henrik <hcmf@es.aau.dk>
2026-05-25 13:52:16 +0200

    hawk energy

--- run_ES.py
+++ run_ES.py
@@ -2,11 +2,11 @@ import csv
 from ES import ES_station
 
 bandwidths = 1000
-energy_budget = [284]#[47,94,142,189,236,284]
-results_file = "SML_energy_ES.csv"
+energy_budget = [47,94,142,189,236,284]
+results_file = "Hawk_energy_ES.csv"
 
-#with open(results_file, 'w', newline='') as f:
-#    csv.writer(f).writerow(['bandwidth_kbps', 'TP', 'FP', 'avg_throughput_kbps'])
+with open(results_file, 'w', newline='') as f:
+    csv.writer(f).writerow(['bandwidth_kbps', 'TP', 'FP', 'avg_throughput_kbps'])
 
 for e in energy_budget:
     es = ES_station("received", bandwidth=bandwidths, energy_budget=e)
