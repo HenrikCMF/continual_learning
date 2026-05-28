9c0c0381975cbb292697a7bf594960b24e523822
Henrik <hcmf@es.aau.dk>
2026-05-28 17:46:39 +0200

    FP test

--- bin/plot_episodes.py
+++ bin/plot_episodes.py
@@ -8,7 +8,7 @@ first_one = next((i for i, e in enumerate(episodes) if e == 1), None)
 if first_one is not None:
     episodes = episodes[first_one + 1:]
 
-window = 10
+window = 20
 x, y = [], []
 for i in range(0, len(episodes) - window + 1):#, window):
     chunk = episodes[i:i + window]
