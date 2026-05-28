93449f9a0ba91a1f8c23e1952958d755fa4b2b6c
Henrik <hcmf@es.aau.dk>
2026-05-28 15:35:42 +0200

    acord pump no quantization

--- run_ES.py
+++ run_ES.py
@@ -1,7 +1,7 @@
 import csv
 from ES import ES_station
 screen="bandwidth"#energy|bandwidth
-results_file = "acord_pump_bandwidth_ES_nodown.csv"
+results_file = "acord_pump_bandwidth_ES_noquant.csv"
 #with open(results_file, 'w', newline='') as f:
 #    csv.writer(f).writerow(['bandwidth_kbps', 'TP', 'FP', 'avg_throughput_kbps'])
 if screen=="energy":
