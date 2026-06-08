6fffaf26dba96c256ef020cfdde62c3f589b2416
Henrik <hcmf@es.aau.dk>
2026-06-08 14:02:17 +0200

    no IoTadapt

--- run_IoT.py
+++ run_IoT.py
@@ -1,12 +1,12 @@
 import csv
 from IoT_device import iot_device
 import time
-screen="energy"#energy|bandwidth
+screen="bandwidth"#energy|bandwidth
  #with open(results_file, 'w', newline='') as f:
 #    csv.writer(f).writerow([
 #        'bandwidth_kbps', 'total_energy', 'time_transmitting_s', 'time_receiving_s',
 #        'total_sent_kb', 'total_received_kb', 'num_inferences', 'avg_throughput_kbps'])
-results_file = "IoT_results_hawk_energy.csv"
+results_file = "pump_no_IoTadapt.csv"
 if screen=="energy":
     bandwidths = 1000
     energy_budget = [28, 85,142,199,255,312]
